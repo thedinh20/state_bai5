30d528a9e248ec9fa4e87942a0c5900871ca8526
thedinh20 <dinhthe12042004@gmail.com>
2025-02-11 10:27:43 +0700

    thay

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
+
 
 export default function App() {
+    const [backgroundColor, setBackgroundColor] = useState('white'); // Trạng thái màu nền
+  
+    const Box = ({ backgroundColor, text }) => {
+      return (
+        <TouchableOpacity 
+          style={[styles.box, { backgroundColor }]} 
+          onPress={() => setBackgroundColor(backgroundColor)} // Thay đổi màu nền khi nhấn
+        >
+          <Text style={styles.text}>{text}</Text>
+        </TouchableOpacity>
+      );
+    };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { backgroundColor }]}>
+      <Box backgroundColor='green'  text='Green'></Box>
+      <Box backgroundColor='blue'   text='Blue'></Box>
+      <Box backgroundColor='brown'  text='Brown'></Box>
+      <Box backgroundColor='yellow' text='Yellow'></Box>
+      <Box backgroundColor='red'    text='Red'></Box>
+      <Box backgroundColor='black'  text='black'></Box>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -17,4 +37,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box : {
+    width: 200,
+    height: 80,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 20,
+
+  },
+  text:{
+    color: '#fff',
+    fontSize: 20,
+  }
+  
 });
